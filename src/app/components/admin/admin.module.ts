32b29994ab0009647be29ac3866dca4d4f5e2be4
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-10-15 09:35:39 -0300

    :construction: Fix modules and components
    Commit final nessa branch, tudo ok.

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashAdminComponent } from './dash-admin/dash-admin.component';
 
-
-
 @NgModule({
   declarations: [
     DashAdminComponent
