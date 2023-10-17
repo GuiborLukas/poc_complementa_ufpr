da0fcdd76c8279fa2470cf3c86ce485b1d15d746
Mateus Maidel <54186179+maidell@users.noreply.github.com>
2023-10-16 23:24:48 -0300

    :construction_worker: Footer is working!

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -3,18 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DashAdminComponent } from './dash-admin/dash-admin.component';
 import { SquareCardComponent } from '../cards/square-card/square-card.component';
 import { RectCardComponent } from '../cards/rect-card/rect-card.component';
+import { CardsModule } from '../cards/cards.module';
 
 
 
 @NgModule({
   declarations: [
-    DashAdminComponent,
-    SquareCardComponent,
-    RectCardComponent
+    DashAdminComponent
   ],
   imports: [
     CommonModule,
-    
+    CardsModule
   ]
 })
 export class AdminModule { 
--- src/app/components/cards/cards.module.ts
+++ src/app/components/cards/cards.module.ts
@@ -8,9 +8,16 @@ import { RectCardComponent } from './rect-card/rect-card.component';
 
 @NgModule({
   declarations: [
+    SquareCardComponent,
+    RectCardComponent
   ],
+  
   imports: [
     CommonModule
+  ],
+  exports: [
+    SquareCardComponent,
+    RectCardComponent
   ]
 })
 export class CardsModule { }
